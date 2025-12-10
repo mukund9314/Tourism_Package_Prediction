66d2207c4d5c16b7daf02e75694e931615c574e8
mukund9314 <mukund9314@gmail.com>
2025-12-10 12:24:36 +0000

    Fix hosting path for GitHub Actions

--- Tourism_Project/hosting/hosting.py
+++ Tourism_Project/hosting/hosting.py
@@ -4,7 +4,7 @@ import os
 api = HfApi(token=os.getenv("HF_TOKEN"))
 
 api.upload_folder(
-    folder_path="/content/Tourism_Package_Prediction/Tourism_Project/deployment",
+    folder_path="Tourism_Project/deployment",
     repo_id="mukund9314/Tourism-Package-Prediction",
     repo_type="space",
     path_in_repo="",
